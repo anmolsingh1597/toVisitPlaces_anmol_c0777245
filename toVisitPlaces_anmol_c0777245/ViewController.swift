8e47d6865f64ec43ba8e78bf72d968508b0ed41e
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-14 09:40:36 -0400

    Initial Commit

---
+++ toVisitPlaces_anmol_c0777245/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_anmol_c0777245
+//
+//  Created by Anmol singh on 2020-06-14.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
