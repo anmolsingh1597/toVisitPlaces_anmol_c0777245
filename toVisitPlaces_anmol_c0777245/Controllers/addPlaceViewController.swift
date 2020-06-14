8e0e6f22f943dd62ec54bfa1ca76e0e98c06cb2d
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-14 10:17:18 -0400

    [Anmol Singh] App: UI designed

---
+++ toVisitPlaces_anmol_c0777245/Controllers/addPlaceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  addPlaceViewController.swift
+//  toVisitPlaces_anmol_c0777245
+//
+//  Created by Anmol singh on 2020-06-14.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class addPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
